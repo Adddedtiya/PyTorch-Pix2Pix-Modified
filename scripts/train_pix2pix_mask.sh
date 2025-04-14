549285ca43da5db3493a7b3adf7e33d5072e7962
Alif Aditya Wicaksono <81549262+Adddedtiya@users.noreply.github.com>
2025-04-14 12:17:41 +0900

    update test

--- scripts/train_pix2pix_mask.sh
+++ scripts/train_pix2pix_mask.sh
@@ -3,11 +3,12 @@ set -ex
     --dataset_mode masking \
     --dataroot "/win/scallop/user/aditya/PelvisRongensDataset/dataset_split" \
     --mask_root "/win/scallop/user/aditya/PelvisRongensDataset/base_masks" \
-    --name mdf_px256_t1 \
+    --name mdf_r9_px256_t521 \
     --model pix2pix \
     --direction AtoB \
+    --netG resnet_9blocks \
     --display_id -1 \
-    --n_epochs 100 --n_epochs_decay 64 \
+    --n_epochs 512 --n_epochs_decay 128 \
     --load_size 256 --crop_size 256 \
     --input_nc 2 --output_nc 1 \
-    --batch_size 16
+    --batch_size 32
