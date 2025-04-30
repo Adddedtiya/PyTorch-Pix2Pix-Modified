05f80fa8064dae280bbd12968891463d043558bd
Alif Aditya Wicaksono <81549262+Adddedtiya@users.noreply.github.com>
2025-04-30 14:27:49 +0900

    updated masks

--- scripts/test_pix7mask.sh
+++ scripts/test_pix7mask.sh
@@ -1,10 +1,13 @@
 set -ex
 /usr/bin/python3 /win/scallop/user/aditya/Pix2Pix/PyTorch-Pix2Pix-Modified/modified_test.py \
-    --name pix7mask_resnet9_512 \
+    --name pix7mask_cte9_512 \
     --dataset_mode curtain \
+    --curtain_type start \
+    --curtain_size 0.5 \
+    --epoch best \
     --dataroot "/win/scallop/user/aditya/PelvisRongensDataset/dataset_split" \
     --model pix7mask \
-    --netG resnet_9blocks \
+    --netG ae_9cte \
     --load_size 256 --crop_size 256 \
     --input_nc 2    --output_nc 1
 
