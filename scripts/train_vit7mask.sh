2e026077be792f3d3b8d1f06db2c1482e3421c0d
Alif Aditya Wicaksono <81549262+Adddedtiya@users.noreply.github.com>
2025-05-12 22:51:59 +0900

    updated hyperparamters

--- scripts/train_vit7mask.sh
+++ scripts/train_vit7mask.sh
@@ -2,7 +2,7 @@ set -ex
 /usr/bin/python3.11 /win/scallop/user/aditya/Pix2Pix/PyTorch-Pix2Pix-Modified/modified_train.py \
     --dataset_mode reconstruction \
     --dataroot "/win/scallop/user/aditya/PelvisRongensDataset/dataset_split" \
-    --name Vit7Mask_256_v2 \
+    --name Vit7Mask_256_v2_t3 \
     --model vit7mask \
     --netG simvit_256_v2 \
     --display_id  -1 \
@@ -10,11 +10,11 @@ set -ex
     --n_epochs  512 --n_epochs_decay 128 \
     --load_size 256 --crop_size 256 \
     --input_nc 1    --output_nc 1 \
-    --patch_size 16 \
+    --patch_size 8 \
     --latent_size 1024 \
-    --encoder_depth 6 \
-    --decoder_depth 9 \
+    --encoder_depth 16 \
+    --decoder_depth 32 \
     --total_heads 8 \
     --vit_ff_size 2048\
-    --batch_size 128 \
-    --lr 5e-4\
+    --batch_size 8 \
+    --lr 5e-4
